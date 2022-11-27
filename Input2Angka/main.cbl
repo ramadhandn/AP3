f9340d62f9ba694f35072f953af23341e8db28d2
nopedawn <62634724+nopedawn@users.noreply.github.com>
2022-11-28 06:56:47 +0700

    Update main.cbl [Sun Nov 28 06:56 AM UTC 2022]

--- Input2Angka/main.cbl
+++ Input2Angka/main.cbl
@@ -7,8 +7,7 @@
 
       ******************************************************************
       * Deskripsi Soal :
-      * Sebuah perusahaan membuka lowongan pekerjaan dengan syarat calon karyawan tersebut diatas 20 tahun maksimal 40 tahun, 
-      * apabila calon karyawan memenuhi kriteria tersebut maka akan lulus pada tahap pertama, jika tidak maka gagal.
+      * Buatlah program input penjumlahan 2 buah angka, lalu menampilkan hasil penjumlahannya, menggunakan bahasa COBOL.
       ******************************************************************
       
        IDENTIFICATION DIVISION.
